50170b4b192d140b84db9618430e1606aac9188b
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-12 22:01:36 -0500

    actualizacion

---
+++ ap3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero a procesar:");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el limite:");
+            int limite = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= limite; i++)
+
+            {  
+                if (i %multiplo == 0)
+                {
+
+                    Console.Write(i + "");
+
+
+                }
+            }
+
+        }
+    }
+
+}
