366ee2655a348a5d05f8184957d00f5afa96c6a5
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-12 21:17:32 -0500

    Agregar archivos de proyecto.

---
+++ ap1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ap1
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese sunota de promedio");
+            double nota = double.Parse(Console.ReadLine());
+            
+           if (nota >= 0 && nota <= 10)
+           {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota >= 5)
+                {
+
+                    Console.WriteLine("reposicion");
+                }
+                else
+                {
+                    Console.WriteLine("reprobado");
+                }
+           }
+           else
+           {
+                Console.WriteLine("la nota no es correcta")
+           }
+
+        }
+
+    }
+
+}
+
