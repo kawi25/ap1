aa43f96b12626873e63fd58e9b45b003e3979e28
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-12 22:16:15 -0500

    actualizacion

---
+++ ap4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int cont = 0;
+
+            Console.WriteLine("ingrese el numero de repeticiones que desea");
+            int limite = int.Parse(Console.ReadLine());
+
+            while (condition)
+            {
+                if (cont == limite)
+                    condition = false;
+                cont++;
+
+            }
+
+        }
+    }
+}
+
