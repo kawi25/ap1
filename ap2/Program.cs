bf91a90bff9198d0f58ce43864e9e53ab10d1e75
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-12 21:47:38 -0500

    actualizacion

---
+++ ap2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("eligio la opcion {0}", option);
+                    break;
+                case 2:
+                    Console.WriteLine("eligio la opcion {0}", option);
+                    break;
+                case 3:
+                    Console.WriteLine("eligio la opcion {0}", option);
+                    break;
+                case 4:
+                    Console.WriteLine("eligio la opcion  {0}", option);
+                    break;
+                case 5:
+                    Console.WriteLine("eligio la opcion {0}", option);
+                    break;
+                default:
+                    Console.WriteLine("esta opcion no existe");
+                    break;
+
+
+
+
+
+            }
+    
+        }
+
+    }
+}
