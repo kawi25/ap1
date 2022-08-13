c4302900bcfd986092789bdc261b953a1d663a1d
FRANCISCO GABRIEL CHICAS BONILLA <SMSS117822@ugb.edu.sv>
2022-08-12 22:32:14 -0500

    actualizacion

---
+++ ap5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+
+                Console.WriteLine("ingrese un numero entero (con 0 se finaliza):");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 100)
+                {
+                    Console.WriteLine("tiene 3 dijitos.");
+                }
+                else if (num >= 10)
+                {
+                    Console.WriteLine("tiene dos digitos.");
+                }
+                while (num != 0) ;
+            }
+        }
+    }
+}
